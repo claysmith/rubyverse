5a4b2c3c8753dee0e6678db715e478507dd485a8
claysmith <clay.smith.r@gmail.com>
2023-07-06 15:46:13 -0400

    latest

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   resources :comments
   devise_for :users
   resources :posts do
-   member do
-    put "like", to: "posts#upvote"
-    put "dislike", to: "posts#downvote"
-   end
-   resources :comments
+  	member do
+  		put "like", to: "posts#upvote"
+  		put "dislike", to: "posts#downvote"
+  	end
+  	resources :comments
   end
-root to: 'posts#index'
+
+  root to: 'posts#index'
 end
